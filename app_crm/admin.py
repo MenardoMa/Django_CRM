7a012c5703711092b08e09849a74a6873b6fc186
Menardo Madiumba <menardotera@gmail.com>
2025-11-09 01:42:50 +0100

    La suite

--- app_crm/admin.py
+++ app_crm/admin.py
@@ -3,4 +3,11 @@ from .models import Record
 
 # Register your models here.
 
-admin.site.register(Record)
+class AdminRecor(admin.ModelAdmin):  
+    #tres important list_display et list_filter
+    #list_display rends les colonnes d'une table dans Admin django visible
+    #list_filter Permet de creer une sorte de filter
+    #class admin.ModelAdmin Permet de personaliser admin django qui est un site
+    list_display = ('pk', 'first_name', 'last_name', 'email', 'phone', 'adress')
+    list_filter  = ('created_at',) 
+admin.site.register(Record, AdminRecor)
